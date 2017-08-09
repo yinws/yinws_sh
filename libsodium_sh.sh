22b4dbf07d6c6fdbd8fcd24b73f2135f4d2888c1
yinws <yinwsddd@gmail.com>
2017-08-09 14:35:54 +0800

    Update libsodium_sh.sh

--- libsodium_sh.sh
+++ libsodium_sh.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update -y
-apt-get install build-essential wget -y
+sudo apt-get install build-essential wget -y
 wget -N --no-check-certificate https://github.com/jedisct1/libsodium/releases/download/1.0.13/libsodium-1.0.13.tar.gz
 tar -xzf libsodium-1.0.13.tar.gz && cd libsodium*
 ./configure --prefix=/usr && make && make install &&  ldconfig
